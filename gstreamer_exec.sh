8e80f401ad392a06aa2562efee1e40aa9d86999b
jangsungLee <hoan12340@naver.com>
2020-05-13 14:12:08 +0900

    Update gstreamer_exec.sh

--- gstreamer_exec.sh
+++ gstreamer_exec.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 # for Linux
 
-gst-launch-1.0 \
-        v4l2src device=/dev/video0 \
-        ! videoconvert ! videoscale ! video/x-raw,width=320,height=240 \
-        ! clockoverlay shaded-background=true font-desc="Sans 38" \
-        ! theoraenc ! oggmux ! tcpserversink host=127.0.0.1 port=8080
+gst-launch-1.0 -v v4l2src ! video/x-raw,width=640,height=480 ! videoconvert ! x264enc ! rtph264pay ! udpsink host=127.0.0.1 port=5000
